4bf729095631fd6403a99cdeef5289fb5ea8daf1
eys35 <eys35@cornell.edu>
2023-10-14 15:18:56 -0400

    Inserted new graphics and re-spaced MacrosView.swift

--- Munch/Munch/MacrosView.swift
+++ Munch/Munch/MacrosView.swift
@@ -12,42 +12,81 @@ struct MacrosView: View {
     @State private var showMacrosView: Bool = false;
     
     var body: some View {
-        VStack{
-            
-            Image("LogoWithBg")
-                .resizable()
-                .frame(width: 150, height: 37.5)
-                .scaledToFit()
-                .aspectRatio(contentMode: .fit)
-                .padding(.vertical)
-                .padding(.bottom)
-            
-            
-            Text("Loading...")
-                .font(.title)
-            
-            HStack {
-                Image("Egg")
-                    .resizable()
-                    .frame(width: 40, height: 45, alignment: .leading)
-                    .padding(.horizontal)
+        ZStack(alignment: .center) {
+
+            VStack{
                 
-                Image("Egg")
+                Image("LogoWithBg")
                     .resizable()
-                    .frame(width: 40, height: 45, alignment: .center)
+                    .frame(width: 150, height: 37.5)
+                    .scaledToFit()
                     .aspectRatio(contentMode: .fit)
-                    .padding(.horizontal)
+                    .padding(.top)
+                    .padding(.top)
+                    .padding(.top)
+                    .padding(.vertical)
                 
+                Spacer()
+
+                Text("Loading...")
+                    .font(.title)
+                    .padding(.bottom)
+                    .padding(.bottom)
                 
-                Image("Egg")
-                    .resizable()
-                    .frame(width: 40, height: 45)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(alignment: Alignment.trailing)
-                    .padding(.horizontal)
+
+                HStack {
+                    Image("Apple")
+                        .resizable()
+                        .frame(width: 40, height: 45, alignment: .leading)
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                    .padding(.bottom)
+                    .padding(.bottom)
+                    .padding(.bottom)
+                    .padding(.bottom)
+                        .padding(.bottom)
+                    
+                    Image("Egg")
+                        .resizable()
+                        .frame(width: 40, height: 45, alignment: .center)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+
+                    
+                    
+                    Image("Sandwich")
+                        .resizable()
+                        .frame(width: 40, height: 45)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(alignment: Alignment.trailing)
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)                    .padding(.bottom)
+
+
+
+                }
+                Spacer()
 
             }
+            
         }
+      
        
     }
 }
