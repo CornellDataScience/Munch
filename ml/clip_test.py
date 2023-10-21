0fbfe2e0c6fe529f990b0a9b130c4a21e55d197b
Abigail Kim <abigailkim@dhcp-vl2041-5220.eduroam.cornell.edu>
2023-10-21 14:45:55 -0400

    Switch to venv, comment out crop
    - Add cheeseburger.jpeg
    - Bryant

--- ml/clip_test.py
+++ ml/clip_test.py
@@ -4,12 +4,16 @@ from PIL import Image
 import numpy as np
 import sys
 
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-img_inp = Image.open(sys.argv[1])
+im = Image.open(sys.argv[1])
+# im_crop = im.crop((int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])))
+
+img_inp = im
 image = preprocess(img_inp).unsqueeze(0).to(device)
-labels = ["Man with weights", "Ronnie Coleman", "Arnold Schwarzenegger"]
+labels = ["Burger", "Bacon Burger",	"Cheese Burger", "Turkey Burger",	"Fish Sandwich",	"Chicken Sandwich",	"Mozzarella Sticks"	"Fries",	"Onion Rings",	"Churros",	"Cheese Pizza",	"Meat Pizza", "Boneless Wings",	"Chicken Parm", "Southwest Chicken Sub", "Turkey Club", "Fried Chicken Sandwich"]
 text = clip.tokenize(labels).to(device)
 
 
