2f181f4cdb4b4fbc99f470e4bd1774a0ab43f2ad
Koji <kjk227@cornell.edu>
2023-10-20 21:36:08 -0400

    Implemented Nutrient get request
    - Created get endpoint
    - Created env file
    - Remove py2neo

--- backend/macros_backend/flask-api/app.py
+++ backend/macros_backend/flask-api/app.py
@@ -4,8 +4,12 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import pandas as pd
 from werkzeug.utils import secure_filename
+
+from dotenv import load_dotenv, find_dotenv
 import os
-from py2neo import Graph
+
+# Load dotenv file
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,9 +20,11 @@ app.config['UPLOADS'] = 'uploads/'
 if not os.path.exists(app.config['UPLOADS']):
     os.makedirs(app.config['UPLOADS'])
 
-# connect to n4jDB instance
-graph = Graph(
-    "bolt://localhost:7687", auth=("neo4j", "12345678"))
+# connect to n4jDB instance using credentials from dotenv file
+
+driver = GraphDatabase.driver(uri=os.environ.get("DATABASE_URL"), auth=(
+    os.environ.get("DATABASE_USERNAME"), os.environ.get("DATABASE_PASSWORD")))
+session = driver.session()
 
 
 class UploadExcel(Resource):
@@ -77,12 +83,12 @@ def populate_db(df1, df2):
         )
 
         # run query
-        graph.run(query)
+        session.run(query)
 
     recipe_pantry = defaultdict(list)
 
     for recipe in (list(df2.columns)[1:]):
-        graph.run(
+        session.run(
             f"MERGE(r:Recipe {{name:'{recipe.lower()}'}})"
         )
 
@@ -98,12 +104,47 @@ def populate_db(df1, df2):
                     f"MERGE (r)-[h:HAS]->(i)"
                     f"SET h.quantity = {quantity}"
                 )
-                graph.run(
+                session.run(
                     query
                 )
 
-    # add resource to endpoint
+
+# add upload excel resource to endpoint
 api.add_resource(UploadExcel, '/upload')
+
+
+class Nutrients(Resource):
+    def get(self, food: str):
+        # Find the macronutrient breakdown of a given food
+
+        query = f"""MATCH path = (root:Recipe{{name:'{food}'}})-[:HAS*]->(macros)
+                    WHERE NOT (macros)-->()
+                    RETURN macros,  
+                    REDUCE(quantity = 1.0, rel in relationships(path) | quantity * toFloat(rel.quantity)) AS quantity"""
+
+        ingredients = session.run(query).data()
+
+        # Check if food is an ingredient in DB
+        if not ingredients:
+            query = f"""MATCH  (root:Ingredient{{name:'{food}'}}) where not (root)-->() return root"""
+            food = session.run(query).data()
+            if food:
+                food = food[0]['root']
+                return {'carbs': food['carbs'], 'fats': food['fats'], 'protein': food['protein']}, 201
+            return {'error': "Food not found"}, 400
+
+        # If food is a recipe in DB
+        carbs, fats, protein = 0.0, 0.0, 0.0
+        for ingredient in ingredients:
+            macros = ingredient['macros']
+            carbs += float(macros['carbs']) * ingredient['quantity']
+            fats += float(macros['fats']) * ingredient['quantity']
+            protein += float(macros['protein']) * ingredient['quantity']
+        return {'carbs': carbs, 'fats': fats, 'protein': protein}, 201
+
+
+# add nutrients resource to endpoint
+api.add_resource(Nutrients, '/nutrients/<string:food>')
 
 
 if __name__ == "__main__":
