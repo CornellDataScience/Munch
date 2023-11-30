22c764b9eed32d036174ba4cdfe359f989172a5b
Bryant Park <bryantpark04@gmail.com>
2023-11-29 21:14:42 -0500

    Move food label loading to outside scope so it only runs once

--- api/app.py
+++ api/app.py
@@ -28,8 +28,13 @@ driver = GraphDatabase.driver(uri=os.environ.get("DATABASE_URL"), auth=(
 session = driver.session()
 
 
-class UploadExcel(Resource):
+# TODO: query database to fetch all food names upon start
+with open("api/foods.txt") as f:
+    food_labels = list(f.read().splitlines())
+    print("Read food labels")
 
+
+class UploadExcel(Resource):
     # process for uploading, reading excel file, and populating database
     def post(self):
 
@@ -83,19 +88,13 @@ class ImageUpload(Resource):
 
 
 class Model(Resource):
-    def __init__(self):
-        # TODO: query database to fetch all food names
-        with open("foods.txt") as f:
-            self.food_labels = list(f.read().splitlines())
-
-
     def get(self, food_id: str):
         filepath = os.path.join(app.config['IMG_UPLOADS'], food_id)
 
         image = Image.open(filepath)
 
         if image:
-            name = classify_img(image, self.food_labels).lower().replace(' ', '%20')
+            name = classify_img(image, food_labels).lower().replace(' ', '%20')
             return {'name': name}, 201
 
         return {'error': "No file found"}, 401
